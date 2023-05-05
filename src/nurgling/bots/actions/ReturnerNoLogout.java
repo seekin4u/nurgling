7c6ea139b0868d2c73bec006ec4e34c26c2cc89b
seekin4u <wagamama47@gmail.com>
2023-05-05 13:39:49 +0300

    Returner with no log out

---
+++ src/nurgling/bots/actions/ReturnerNoLogout.java
@@ -0,0 +1,40 @@
+package nurgling.bots.actions;
+
+import haven.Gob;
+import nurgling.NAlias;
+import nurgling.NGameUI;
+import nurgling.NUtils;
+import nurgling.tools.Finder;
+
+public class ReturnerNoLogout implements Action {
+    @Override
+    public Results run ( NGameUI gui )
+            throws InterruptedException {
+        if(!isShip) {
+            NUtils.command(new char[]{'a', 'h', 'h'});
+            NUtils.waitEvent(() -> NUtils.getProg() >= 0, 200);
+            NUtils.waitEvent(() -> NUtils.getProg() < 0, 10000);
+            NUtils.waitEvent(() -> NUtils.getGob(NUtils.getGameUI().map.player().id) != null, 2000);
+        }
+        else
+        {
+            Gob ship = Finder.findObject(new NAlias("snekkja", "knarr"));
+            if(ship!=null){
+                new SelectFlowerAction(ship,"Travel to home dock", SelectFlowerAction.Types.Gob).run(gui);
+                NUtils.waitEvent(() -> NUtils.getProg() >= 0, 200);
+                NUtils.waitEvent(() -> NUtils.getProg() < 0, 10000);
+                NUtils.waitEvent(() -> Finder.findObject(new NAlias("knarrdock"))!=null, 10000);
+            }
+        }
+        return new Results ( Results.Types.SUCCESS );
+    }
+
+    public ReturnerNoLogout(boolean isShip) {
+        this.isShip = isShip;
+    }
+
+    public ReturnerNoLogout(){
+    }
+
+    boolean isShip = false;
+}
