f91460bf17cd3d0dbc8962f4000b202aa7afc0eb
seekin4u <wagamama47@gmail.com>
2023-07-08 03:09:27 +0300

    Gate bot fixed

--- src/nurgling/bots/actions/ActivateAndReturn.java
+++ src/nurgling/bots/actions/ActivateAndReturn.java
@@ -21,42 +21,32 @@ public class ActivateAndReturn implements Action {
         objectToActivate = Finder.findObject(name);
 
         while (true) {
+            if(Close())
+                Return(gui);
             if(CheckForFriend(gui)){
-                waitEvent(()->Open(), 100);
-                Return(gui);
-                Thread.sleep(10000);
-                waitEvent(()->Close(), 100);
-                Return(gui);
+                if(Open())
+                    Return(gui);
+                Thread.sleep(4000);
             }
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         }
     }
-    private boolean DistanceToFriend(){
-        ArrayList<Gob> friend = Finder.findAllinDistance(new NAlias("borka"), 5000);
-        return true;
-    }
     private boolean CheckForFriend(NGameUI gui){
-        //objects in area!
         ArrayList<Gob> gobs = Finder.findObjects(new NAlias("borka"));
         for (Gob gob : gobs) {
             if(gob.id!=gui.map.player().id) {
                 int group = KinInfo.getGroup(gob);
-                Color color;
-
                 if (group == 0 || group == -1 && gob.getattr(NGobHealth.class) == null) {
-                    //держим ворота закрытыми
                     return false;
                 } else if (KinInfo.getGroup(gob) == 2) {
-                    //держим ворота закрытыми
                     return false;
-                } else if(KinInfo.getGroup(gob) == 1){
-                    //проверяем на зеленый цвет, открываем.
+                } else if(KinInfo.getGroup(gob) == 1) {
                     synchronized ( NUtils.getGameUI().ui.sess.glob.oc ){
                         double dist = NUtils.getGameUI().map.player ().rc.dist ( gob.rc );
-                        if (dist > 5000) return false;
+                        if (dist < 150)
+                            return true;
                     }
-
-                    return true;
+                    return false;
                 }
             }
         }
@@ -66,38 +56,37 @@ public class ActivateAndReturn implements Action {
         Coord2d start = Finder.findObject(new NAlias("runestone")).rc;
         PathFinder pf = new PathFinder ( gui, start );
         pf.run ();
+        Thread.sleep(400);
     }
-    private boolean Open(){
+    private boolean Open() throws InterruptedException {
         objectToActivate = Finder.findObject(name);
         if(objectToActivate == null){
-            //return new Results ( Results.Types.NO_WORKSTATION );
             return false;
         }
         long att = objectToActivate.getModelAttribute();//closed attr=2
-        if(att==2){
+        if(att==2) {//if gates are closed - open them
             NUtils.activate(objectToActivate);
-        }
-        return true;
+            Thread.sleep(400);
+            return true;
+        } else return false;//bro fuck off they are already opened somehow. Get rekt.
     }
 
-    private boolean Close(){
+    private boolean Close() throws InterruptedException {
         objectToActivate = Finder.findObject(name);
         if(objectToActivate == null){
-            //return new Results ( Results.Types.NO_WORKSTATION );
             return false;
         }
-            long att = objectToActivate.getModelAttribute();//closed attr=2
-        if(att==1){
+        long att = objectToActivate.getModelAttribute();//closed attr=2
+        if(att==1) {//opened so we need to close them
             NUtils.activate(objectToActivate);
-        }
-        return true;
+            Thread.sleep(400);
+            return true;
+        } else return false;
     }
     public ActivateAndReturn(
             NAlias name
     ) {
         this.name = name;
     }
-    
     NAlias name;
-    
 }
